8238ba57c4736cc90ca2ffda4f596b2526c68145
talha309 <tiseoexpert@gmail.com>
2024-11-17 19:14:36 +0500

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,132 @@
+# basic operator (arithmatic)
+#a = 34
+#b =  23
+#print ("a + b=", a + b)
+#print ("a - b=",a-b)
+#print ("a * b=",a*b)
+#print ("a // b=",a//b)
+#print ("a % b=", a%b )
+#a = int(input("enter:"))
+#b = int(input("enter:"))
+#print ("a ** b:", a ** b)
+
+# if statmenet 
+#marks = int(input("enter your marks:"))
+#if marks >=80:
+    #print("you got A")
+#elif marks>=65:
+   # print("you got B")
+#elif marks>=50:
+    #print("you got C")
+#elif marks>=41:
+   # print("you got D")
+#else:
+   # print("you got F")
+
+#for loop 
+#for i in range(0,15,1):
+    #print(i)
+# while loop 
+#i = 1
+#while i <=15:
+   # print(i)
+   # if i==10:
+        #break 
+    #i =i+1
+    
+#a = 1
+#while a < 20:
+        #print(a)
+        #if a==10:
+            #print("hello world")
+            #a=a+5
+            #continue
+        #a= a+3
+
+#cars = ["bmw,ford,civci"]
+#x=cars[0]
+#cars="toyta"
+#cars.append("honda")
+#x=len(cars)
+#for x in cars(1,14,2):
+    #pprint(x)
+
+#fruits = ['apple', 'bananan', 'cherry']
+#print(fruits[0])
+#fruits = ['apple', 'bananan', 'cherry']
+#(len(fruits))
+#Write a program that checks if a given number is positive, negative, or zero.
+#a = int(input("enter number :"))
+#if a>0 :
+   # print("number is positive.")
+#elif a<0:
+   # print("number is negative.")
+#else:
+   # print("number is zero.")
+#Take a user’s age as input and display whether they are a minor, adult, or senior citizen.
+#a = int(input("enter age :"))
+#if a<18:
+    #print("Your are minor citiizen")
+#elif a<=40:
+    #print("Your are adult citizen")
+#else:
+   # print ("Your are senior citizen")
+#Write a program that checks if a given year is a leap year.
+#a = int(input("enter Year: "))
+#if a%4==0:
+    #print ("leap year")
+#else:
+   # print ("not leap year")
+#Take an integer and check if it’s even or odd.
+#a = int(input("enter integer: "))
+#if a%2==0:
+   # print("even number")
+#else:
+   # print("odd number")
+#Ask the user for a grade percentage and display the corresponding letter grade (A, B, C, D, F).
+#grade_percentage = float(input("enter your grade percentage :"))
+#if grade_percentage >=80:
+    #print("your letter grade is A")
+#elif grade_percentage >=65:
+   # print("your letter grade is B")
+#elif grade_percentage >=55:
+   # print("your letter grade is C")
+#elif grade_percentage >40:
+   # print("your letter grade is D")
+#else:
+   # print("your letter grade is F")
+
+#Write a program to find the largest of two numbers
+#float = "num1,num2"
+#num1 = input("enter first number:")
+#num2 = input("enter second number:")
+#if num1>num2:
+   # print("largest is first number.")
+#elif num1<num2:
+    #print("largest is second number.")
+#else:
+    #print("first & second number is equal.")
+
+#Take three sides of a triangle as input and check if they form a valid triangle.
+#a= int(input("eanter side value"))
+#b= int(input("eanter side value"))
+#c= int(input("eanter side value"))
+#if a+b>c:
+  # print("this is valid triangle")
+#elif a+c>b:
+    #print("this is valid triangle")
+#elif b+c>a:
+      # print("this is valid triangle")
+#else: 
+  # print("this is not valid triangle")
+
+ # Write a program to determine if a given character is a vowel or consonant
+#pass
+#Check if a given number is a multiple of both 3 and 5.
+a=int(input("enter number:"))
+if a%3==0:
+   print("multiple of 3")
+elif a%5==0:
+   print("multiple of 5")
+else:
+   print("not multiple of 3 & 5")
